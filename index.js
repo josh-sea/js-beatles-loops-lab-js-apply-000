6971b26ff370baabff42df8ce55a923a01990f0f
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-08-29 10:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+ for (let i=0; i < musicians.length; i++) {
+   var newArray = [];
+   newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+   return newArray
+  }
+}
